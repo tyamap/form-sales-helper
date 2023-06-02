68f4b4945f3e7971f203a0db401619a6be285535
tyamap <tom333my@outlook.com>
2023-06-03 02:20:04 +0900

    :tada: initialize!

---
+++ entities/BasicInfo.ts
@@ -0,0 +1,15 @@
+export type BasicInfo = {
+  familyName: string;
+  givenName: string;
+  email: string;
+  tel: string;
+  organization: string;
+}
+export const basicInfoDisplay = {
+  familyName: '姓',
+  givenName: '名',
+  email: 'メールアドレス',
+  tel: '電話番号',
+  organization: '会社名',
+}
+
---
+++ entities/Templates.ts
@@ -0,0 +1,10 @@
+export type Templates = {
+  template1: string
+  template2: string
+  template3: string
+}
+export const templatesDisplay = {
+  template1: 'テンプレート1',
+  template2: 'テンプレート2',
+  template3: 'テンプレート3',
+}
