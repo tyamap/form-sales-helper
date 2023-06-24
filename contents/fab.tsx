89e03c5c03e069fa913f68d5febd88764ea7ae2f
tyamap <tom333my@outlook.com>
2023-06-24 16:02:25 +0900

    feat: 選択したテキストを保持し、インプットクリック時に注入する

--- contents/fab.tsx
+++ contents/fab.tsx
@@ -1,6 +1,6 @@
 import cssText from "data-text:~style.css"
 import type { PlasmoCSConfig } from "plasmo"
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 export const getStyle = () => {
   const style = document.createElement("style")
@@ -13,28 +13,30 @@ export const config: PlasmoCSConfig = {
   all_frames: true
 }
 
+const SELECTED_TEXT_ID = 'fsh-selected-text'
+
 const fab = () => {
   const [open, setOpen] = useState(false)
-  const [showCopied, setShowCopied] = useState(false)
 
   // ボタンクリックしたら内容をコピーする
   const copyContent = (value: string) => {
-    navigator.clipboard.writeText(value).then(
-      () => {
-        setShowCopied(true)
-        setTimeout(() => {
-          setShowCopied(false)
-        }, 2000)
-      })
+    if (document.querySelector(`#${SELECTED_TEXT_ID}`)) {
+      document.querySelector(`#${SELECTED_TEXT_ID}`)?.remove()
+    }
+    // テキストを保持する要素を作成
+    const elem = document.createElement('div')
+    elem.id = SELECTED_TEXT_ID
+    elem.style.display = 'none'
+    elem.textContent = value
+    document.body.appendChild(elem)
   }
 
   return (
     <div className="fixed bottom-4 right-4">
       <div className={`absolute bottom-0 right-0 w-64 h-64 p-2 bg-white rounded shadow-lg transform transition-all duration-300 ${open ? 'scale-100' : 'scale-0'}`}>
-        <button className="ML-2" onClick={() => setOpen(false)}>
+        <button className="ml-2" onClick={() => setOpen(false)}>
           X
         </button>
-        <span className={`text-teal-500 ${showCopied ? 'opacity-100' : 'opacity-0'} transition ml-2`}>コピーしました</span>
         <div className="flex flex-col justify-center items-center h-full">
           <button onClick={() => copyContent('ACTION')}>
             ACTION
@@ -51,3 +53,14 @@ const fab = () => {
 };
 
 export default fab;
+
+window.addEventListener("load", () => {
+  document.querySelectorAll('input,textarea').forEach((elm) => {
+    elm.addEventListener("focus", (e) => {
+      const value = document.querySelector(`#${SELECTED_TEXT_ID}`).textContent
+      console.log(document.querySelector(`#${SELECTED_TEXT_ID}`))
+      const target = e.target as HTMLInputElement
+      target.value = value
+    })
+  })
+})
